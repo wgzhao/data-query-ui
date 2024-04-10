2d8c932d130733d83633a427ef0ae1b2b79d934e
wgzhao <wgzhao@gmail.com>
2024-04-10 08:58:16 +0800

    Add staging env

--- src/util/request.ts
+++ src/util/request.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import router from '@/router';
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
-// axios.defaults.headers.common['token'] = import.meta.env.VITE_API_TOKEN;
 
 const token = localStorage.getItem('token');
 if (token !== null) {
