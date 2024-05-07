9089e07546d7075f49ca2a19065380f3b268c459
wgzhao <wgzhao@gmail.com>
2024-05-07 17:15:38 +0800

    Update dependencies and add new 404 page

---
+++ src/pages/[...all].vue
@@ -0,0 +1,11 @@
+<!-- 404 page -->
+<template>
+    <v-empty-state
+  headline="Whoops, 404"
+  title="Page not found"
+  text="The page you were looking for does not exist"
+></v-empty-state>
+</template>
+
+<script setup type="ts">
+</script>
